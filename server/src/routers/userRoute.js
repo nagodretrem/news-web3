b23197dbe0457686598d2dba9a339656f24aba49
Mert <nagodretrem@gmail.com>
2023-01-13 08:42:21 +0300

    Graduation project almost done

--- server/src/routers/userRoute.js
+++ server/src/routers/userRoute.js
@@ -6,8 +6,16 @@ const Response = require("../utils/response");
 router.get("/users", async (req, res) => {
   try {
     const users = await User.find();
-    console.log("users", users);
     return new Response(users, "Users").success(res);
+  } catch (err) {
+    APIError(err.message, 500, "Users Fetch Failed").send(res);
+  }
+});
+
+router.get("/user/:id", async (req, res) => {
+  try {
+    const user = await User.findOne({ _id: req.params.id });
+    return new Response(user, "Users").success(res);
   } catch (err) {
     APIError(err.message, 500, "Users Fetch Failed").send(res);
   }
@@ -15,6 +23,7 @@ router.get("/users", async (req, res) => {
 
 router.patch("/user/:_id", async (req, res) => {
   try {
+    console.log(req.params._id);
     const user = await User.findByIdAndUpdate(req.params._id, {
       $set: req.body,
     });
